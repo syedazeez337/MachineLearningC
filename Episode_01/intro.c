4447df69f2b91d1c25b381e5ddf59d69fe22b53e
syedazeez337 <67051473+syedazeez337@users.noreply.github.com>
2023-05-23 21:54:59 +0530

    Add files via upload

---
+++ Episode_01/intro.c
@@ -0,0 +1,78 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+float train[][2] = {
+	{0,0},
+	{1,2},
+	{2,4},
+	{3,6},
+	{4,8},
+};  //parameters
+
+#define train_count (sizeof(train)/sizeof(train[0]))
+
+// y = x * w;
+float rand_float(void)
+{
+	return (float) rand() / (float) RAND_MAX;
+}
+
+float cost(float w)
+{
+	float result = 0.0f;
+	size_t n = train_count;
+	for(size_t i = 0; i < n; ++i)
+	{
+		float x = train[i][0];
+		float y = x * w;
+		float d = y - train[i][1];
+		result += d*d;
+	}
+	result /= n;
+	return result;
+}
+
+float dcost(float w)
+{
+	float result = 0.0f;
+	size_t n = train_count;
+	for(size_t i = 0; i < n; ++i)
+	{
+		float x = train[i][0];
+		float y = train[i][1];
+		result += 2 * (x*w - y) * x;
+	}
+	result /= n;
+	return 0;
+}
+
+int main(void)
+{
+	//srand(time(0));
+	srand(69);
+	float w = rand_float() * 10.0f;
+
+	float eps = 1e-3;
+	float rate = 1e-3;
+
+	for(size_t i = 0;i < 100; ++i)
+	{
+		float dcost = (cost(w - eps) - cost(w))/eps;
+	    printf("%f\n", cost(w));
+	    w -= rate*dcost;
+	    printf("%f\n", cost(w));
+	}
+	printf("X--------------X\n");
+
+	printf("%f", dcost(w));
+
+	return 0;
+}
+/*
+printf("%f\n", cost(w - eps));
+	printf("%f\n", cost(w - eps*2));
+	printf("%f\n", dcost);
+*/
